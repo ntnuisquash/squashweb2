3a8a5c964611a58bd78133d55a6dcddb70b850d7
Casper Andreassen <caspersa@stud.ntnu.no>
2023-02-22 16:34:10 +0100

    Setup tailwind 🌝

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,11 @@ import "./App.css";
 function App() {
   const [count, setCount] = useState(0);
 
-  return <div className="App"></div>;
+  return (
+    <div>
+      <h1 className="text-3xl">Hello Squash!</h1>
+    </div>
+  );
 }
 
 export default App;
